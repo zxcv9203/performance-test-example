d5b48ce632fac74509576885cfc3864b45848c7a
yongckim <zxcv9203@naver.com>
2024-07-25 21:50:29 +0900

    :heavy_plus_sign: 캐싱을 위한 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,6 +29,10 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    // cache
+    implementation("org.ehcache:ehcache")
+    implementation("org.hibernate:hibernate-ehcache")
 }
 
 kotlin {
