74b0ba6e5bd3051d77adbfe442cf458f95564855
yongckim <zxcv9203@naver.com>
2024-08-02 22:59:03 +0900

    :recycle: refactor : 리스트를 페이지 객체로 반환하도록 변경

--- src/main/kotlin/org/example/performancetestexample/service/PostService.kt
+++ src/main/kotlin/org/example/performancetestexample/service/PostService.kt
@@ -2,6 +2,7 @@ package org.example.performancetestexample.service
 
 import org.example.performancetestexample.domain.Post
 import org.example.performancetestexample.repository.PostRepository
+import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
 
@@ -9,5 +10,5 @@ import org.springframework.stereotype.Service
 class PostService(
     private val postRepository: PostRepository
 ) {
-    fun findAll(pageable: Pageable): List<Post> = postRepository.findAll(pageable).toList()
+    fun findAll(pageable: Pageable): Page<Post> = postRepository.findAll(pageable)
 }
